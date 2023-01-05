6763c1d06137c006a1d0abe6cd480b2b45df2d03
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-01-05 18:55:21 +0300

    build featured gamers dynamically

--- myscript.js
+++ myscript.js
@@ -11,3 +11,58 @@ closer.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navigation2.classList.remove('active');
 })
+
+const featured = [
+    {
+      name: 'Pat Wach',
+      image: 'images/pat-Wach.jpg',
+      country: 'Kenya',
+      expertise: 'Call of Duty',
+    },
+    {
+      name: 'Queen Arrow',
+      image: 'images/Queen-Arrow.jpg',
+      country: 'Kenya',
+      expertise: 'Halo',
+    },
+    {
+      name: 'Terrence',
+      image: 'images/Terrence.jpg',
+      country: 'South Africa',
+      expertise: 'Pub G',
+    },
+    {
+      name: 'Hirsi',
+      image: 'images/Hirsi.png',
+      country: 'South Africa',
+      expertise: 'Apex Legends',
+    },
+    {
+      name: 'Damien Kriya',
+      image: 'images/Brian.jpg',
+      country: 'India',
+      expertise: 'DOOM Eternal',
+    },
+    {
+      name: 'Brian Beast',
+      image: 'images/Brian.jpg',
+      country: 'Kenya',
+      expertise: 'Counter Strike',
+    },
+]
+
+const gamers = document.getElementById('gamers');
+for (let i = 0; i < featured.length; i += 1) {
+    gamers.innerHTML += `
+    <div class="gamer-entry">
+      <img id="chess-bg" src="images/blank-chess-board.gif">
+      <img src="${featured[i].image}" alt="gamer">
+      <div class="gamer-details">
+         <h4>${featured[i].name}</h4>
+         <p class="country">${featured[i].country}</p>
+         <div id="feat-separator">
+         <p>${featured[i].expertise}</p>
+      </div>
+  </div>
+    `;
+  }
