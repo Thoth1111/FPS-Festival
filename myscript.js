0e77f0da2fc89d9a9b51518dae0dcdd53e345be3
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-01-05 22:33:25 +0300

    Fix linter errors

--- myscript.js
+++ myscript.js
@@ -3,57 +3,57 @@ const navigation2 = document.getElementById('nav2');
 const closer = document.getElementById('close-btn');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navigation2.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  navigation2.classList.toggle('active');
 });
 
 closer.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navigation2.classList.remove('active');
-})
+  hamburger.classList.remove('active');
+  navigation2.classList.remove('active');
+});
 
 const featured = [
-    {
-      name: 'Pat Wach',
-      image: 'images/pat-Wach.jpg',
-      country: 'Kenya',
-      expertise: 'Call of Duty',
-    },
-    {
-      name: 'Queen Arrow',
-      image: 'images/Queen-Arrow.jpg',
-      country: 'Kenya',
-      expertise: 'Halo',
-    },
-    {
-      name: 'Terrence',
-      image: 'images/Terrence.jpg',
-      country: 'South Africa',
-      expertise: 'Pub G',
-    },
-    {
-      name: 'Hirsi',
-      image: 'images/Hirsi.png',
-      country: 'South Africa',
-      expertise: 'Apex Legends',
-    },
-    {
-      name: 'Damien Kriya',
-      image: 'images/Brian.jpg',
-      country: 'India',
-      expertise: 'DOOM Eternal',
-    },
-    {
-      name: 'Brian Beast',
-      image: 'images/Brian.jpg',
-      country: 'Kenya',
-      expertise: 'Counter Strike',
-    },
-]
+  {
+    name: 'Pat Wach',
+    image: 'images/pat-Wach.jpg',
+    country: 'Kenya',
+    expertise: 'Call of Duty',
+  },
+  {
+    name: 'Queen Arrow',
+    image: 'images/Queen-Arrow.jpg',
+    country: 'Kenya',
+    expertise: 'Halo',
+  },
+  {
+    name: 'Terrence',
+    image: 'images/Terrence.jpg',
+    country: 'South Africa',
+    expertise: 'Pub G',
+  },
+  {
+    name: 'Hirsi',
+    image: 'images/Hirsi.png',
+    country: 'South Africa',
+    expertise: 'Apex Legends',
+  },
+  {
+    name: 'Damien Kriya',
+    image: 'images/Brian.jpg',
+    country: 'India',
+    expertise: 'DOOM Eternal',
+  },
+  {
+    name: 'Brian Beast',
+    image: 'images/Brian.jpg',
+    country: 'Kenya',
+    expertise: 'Counter Strike',
+  },
+];
 
 const gamers = document.getElementById('gamers');
 for (let i = 0; i < featured.length; i += 1) {
-    gamers.innerHTML += `
+  gamers.innerHTML += `
     <div class="gamer-entry">
       <img id="chess-bg" src="images/blank-chess-board.gif">
       <img src="${featured[i].image}" alt="gamer">
@@ -65,4 +65,4 @@ for (let i = 0; i < featured.length; i += 1) {
       </div>
   </div>
     `;
-  }
+}
